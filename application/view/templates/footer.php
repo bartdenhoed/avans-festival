6a9e4d43fb2be20b8dd23e75360350afc4908902
Bart den Hoed <bartdhoed@gmail.com>
2017-02-02 21:43:32 +0100

    Add github url

--- application/view/templates/footer.php
+++ application/view/templates/footer.php
@@ -1,6 +1,6 @@
 </div>
     <footer>
-        <p>Copyright &copy; 2017 - Versie <?= VERSION; ?> - Bart den Hoed - <a href="https://github.com/bartdenhoed/" target="_blank">Github</a> - <a href="http://avans-festival.bartproductions.nl" target="_blank">Live demo</a></p>
+        <p>Copyright &copy; 2017 - Versie <?= VERSION; ?> - Bart den Hoed - <a href="https://github.com/bartdenhoed/avans-festival" target="_blank">Github</a> - <a href="http://avans-festival.bartproductions.nl" target="_blank">Live demo</a></p>
     </footer>
 </body>
 </html>
